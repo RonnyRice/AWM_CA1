8aed9a433079774890d2fc67e424c6a5f99e11ac
RonnyRice <82329993+RonnyRice@users.noreply.github.com>
2023-11-12 20:35:13 +0000

    hello

--- geoDjangoProject/settings.py
+++ geoDjangoProject/settings.py
@@ -15,15 +15,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -31,8 +22,13 @@ STATICFILES_DIRS = [
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "qk(t&0fh*7wo=x1f-8$^@#&mme$#e3hj4mrp2s-&-13@ty+g43"
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+os.environ['PROJ_LIB'] = f"{os.environ.get('CONDA_PREFIX', '')}/share/proj"
+os.environ['GDAL_DATA'] = f"{os.environ.get('CONDA_PREFIX', '')}/share"
 
 # Application definition
 
@@ -134,6 +130,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -153,6 +154,11 @@ LEAFLET_CONFIG = {
     'SCALE': None,
     'OPACITY': 0.5,
 }
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
 DEPLOY_SECURE = False
 
 
