c8f639e2d1599790b1bf9747d9639fd4e80e1620
RonnyRice <82329993+RonnyRice@users.noreply.github.com>
2023-11-12 20:17:31 +0000

    hello

--- geoDjangoProject/settings.py
+++ geoDjangoProject/settings.py
@@ -15,6 +15,15 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -22,13 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "qk(t&0fh*7wo=x1f-8$^@#&mme$#e3hj4mrp2s-&-13@ty+g43"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
-os.environ['PROJ_LIB'] = f"{os.environ.get('CONDA_PREFIX', '')}/share/proj"
-os.environ['GDAL_DATA'] = f"{os.environ.get('CONDA_PREFIX', '')}/share"
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -95,7 +99,7 @@ DATABASES = {
         "ENGINE": "django.contrib.gis.db.backends.postgis",
         "NAME": "gis",
         "USER": "docker",
-        "PASSWORD": "password123",
+        "PASSWORD": "docker",
         "HOST": "localhost",
         "PORT": '25432',
     }
@@ -130,11 +134,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -154,11 +153,6 @@ LEAFLET_CONFIG = {
     'SCALE': None,
     'OPACITY': 0.5,
 }
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-
 DEPLOY_SECURE = False
 
 
@@ -175,7 +169,7 @@ else:
 if DEPLOY_SECURE:
     DEBUG = False
     TEMPLATES[0]["OPTIONS"]["debug"] = False
-    ALLOWED_HOSTS = ['ronliquit.com',  'localhost',]
+    ALLOWED_HOSTS = ['ronliquit.com',  'localhost', '20.56.37.245']
     CSRF_COOKIE_SECURE = True
     SESSION_COOKIE_SECURE = True
 else:
