2646120b4b6525ad1737c58bed8899514abb7d46
RonnyRice <82329993+RonnyRice@users.noreply.github.com>
2023-11-14 15:26:58 +0000

    hello

--- itinerary/admin.py
+++ itinerary/admin.py
@@ -1,3 +1,12 @@
+from django.contrib import admin
+
+from itinerary.models import Profile, ItineraryPlan, ItineraryEvent, User
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(ItineraryPlan)
+admin.site.register(ItineraryEvent)
+
 
